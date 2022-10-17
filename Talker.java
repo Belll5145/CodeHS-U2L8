b9037ab6faa6543dc59f618bbf58ec8e4dd30aa1
Belll5145 <113708748+Belll5145@users.noreply.github.com>
2022-10-17 15:55:26 -0700

    Add files via upload

---
+++ Talker.java
@@ -0,0 +1,48 @@
+public class Talker
+{
+    private String text;
+    
+    // Constructor
+    public Talker(String startingText)
+    {
+        text = startingText;
+    }
+    
+    // Returns the text in all uppercase letters
+    // Find a method in the JavaDocs that
+    // will allow you to do this with just
+    // one method call
+    public String yell()
+    {
+        String yelling = text.toUpperCase();
+        return yelling;
+    }
+    
+    // Returns the text in all lowercase letters
+    // Find a method in the JavaDocs that
+    // will allow you to do this with just
+    // one method call
+    public String whisper()
+    {
+        String whispering = text.toLowerCase();
+        return whispering;
+    }
+    
+    // Reset the instance variable to the new text
+    public void setText(String newText)
+    {
+        text = newText;
+    }
+    
+    // Returns a String representatin of this object
+    // The returned String should look like
+    // 
+    // I say, "text"
+    // 
+    // The quotes should appear in the String
+    // text should be the value of the instance variable
+    public String toString()
+    {
+       return "I say, \""+ text+"\"";
+    }
+}
